f7eb04707940321e8566740b51d1a794e386967e
Luxter <lukasz.cyran92@gmail.com>
2024-01-13 20:28:44 +0100

    Implemented e-greedy action selection.

--- src/apps/train/dqn_train.py
+++ src/apps/train/dqn_train.py
@@ -1,6 +1,7 @@
 # Training script for DQN based on CleanRL implementation
 # https://github.com/vwxyzjn/cleanrl/blob/master/cleanrl/dqn.py
 
+import random
 from typing import Final
 
 import fire
@@ -17,7 +18,7 @@ from lib.seeding import set_seed
 class QNetwork(nn.Module):
     def __init__(self, single_observation_space_shape, single_action_space_shape):
         super().__init__()
-        logger.debug(f"{single_observation_space_shape}, {single_action_space_shape}")
+
         self.network = nn.Sequential(
             nn.Linear(np.array(single_observation_space_shape).prod(), 120),
             nn.ReLU(),
@@ -30,46 +31,63 @@ class QNetwork(nn.Module):
         return self.network(x)
 
 
-def main(total_timesteps: int = 500000):
+def linear_schedule(start_eps: float, end_eps: float, duration: int, t: int):
+    slope = (end_eps - start_eps) / duration
+    return max(start_eps + slope * t, end_eps)
+
+
+def main(
+    total_timesteps: int = 500000,
+    start_eps: float = 1.0,
+    end_eps: float = 0.05,
+    exploration_fraction: float = 0.5,
+    buffer_size: int = 10000,
+):
     seed = 0
     set_seed(seed)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # This needs to be a vectorized environment because replay buffer expects batched data
-    env = gym.vector.SyncVectorEnv([lambda: gym.make("Acrobot-v1")])
+    envs = gym.vector.SyncVectorEnv([lambda: gym.make("Acrobot-v1")])
 
-    q_network = QNetwork(env.single_observation_space.shape, env.single_action_space.n)
-
-    buffer_size: Final[int] = 10000
+    q_network = QNetwork(
+        envs.single_observation_space.shape, envs.single_action_space.n
+    )
 
     replay_buffer = ReplayBuffer(
         buffer_size,
-        env.observation_space,
-        env.action_space,
+        envs.single_observation_space,
+        envs.single_action_space,
         device=device,
         handle_timeout_termination=False,
     )
 
     # Start
-    observations, _ = env.reset(seed=seed)
+    obs, _ = envs.reset(seed=seed)
     for global_step in range(total_timesteps):
         logger.info(f"Global step: {global_step}")
 
         # Action logic
-        q_values = q_network(torch.Tensor(observations))
-        actions = torch.argmax(q_values, dim=1).numpy()
-
-        next_observations, rewards, terminations, truncations, infos = env.step(actions)
-
-        replay_buffer.add(
-            observations, next_observations, actions, rewards, terminations, infos
+        epsilon = linear_schedule(
+            start_eps, end_eps, exploration_fraction * total_timesteps, global_step
         )
+        if random.random() < epsilon:
+            actions = envs.action_space.sample()
+        else:
+            q_values = q_network(torch.Tensor(obs))
+            actions = torch.argmax(q_values, dim=1).numpy()
+
+        next_obs, rewards, terminations, truncations, infos = envs.step(actions)
+
+        replay_buffer.add(obs, next_obs, actions, rewards, terminations, infos)
+
+        obs = next_obs
 
     if terminations or truncations:
-        observations, _ = env.reset(seed=seed)
+        obs, _ = envs.reset(seed=seed)
 
-    env.close()
+    envs.close()
 
 
 if __name__ == "__main__":
